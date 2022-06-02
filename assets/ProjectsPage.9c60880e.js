e2f3ae79d6d6c784d83c9b9845aac17f2442f9e9
seloxyyz <65386358+seloxyyz@users.noreply.github.com>
2022-06-02 09:13:32 +0200

    added localok.co

---
+++ assets/ProjectsPage.9c60880e.js
@@ -0,0 +1,1 @@
+import{i as s,c as r,t as o}from"./index.000fab0d.js";import{P as c}from"./Projects.c5c1776b.js";const l=o('<div class="flex flex-col items-center h-screen"><div class="mt-32"></div></div>'),n=()=>(()=>{const e=l.cloneNode(!0),t=e.firstChild;return s(t,r(c,{})),e})();export{n as default};
