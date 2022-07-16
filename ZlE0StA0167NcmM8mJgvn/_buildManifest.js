8156184835de0fe4240698c694d8c8656cf0dd94
seloxyyz <65386358+seloxyyz@users.noreply.github.com>
2022-07-16 12:18:22 +0200

    revert

--- ZlE0StA0167NcmM8mJgvn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-d19f1df06aef2cec.js","static/chunks/pages/index-4d5b1554d46ccce8.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
